44b6f5b5f40cd87b9e9b76580b8045cf175c9b54
Russell Moore <russellwmoore@gmail.com>
2018-09-24 18:06:27 -0400

    made tables

--- models.js
+++ models.js
@@ -1,42 +1,55 @@
-const Sequelize = require('sequelize')
-const db = new Sequelize('postgres://localhost:5432/plantr')
-
-module.exports = db
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/plantr');
 
 const Gardener = db.define('gardners', {
-    name : {
-        type: Sequelize.STRING,
-      allownull: false
-    },
-    age: {
-        type: Sequelize.INTEGER,
-      allowNull: false
-    }
-})
+  name: {
+    type: Sequelize.STRING,
+    allownull: false,
+  },
+  age: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+});
 
 const Plot = db.define('plots', {
-    size: {
-        type: Sequelize.integer,
-        allowNull: false
-    },
-    shaded: {
-        type: Sequelize.BOOLEAN,
-        allowNull: false
-    }
-})
+  size: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  shaded: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+  },
+});
 
 const Vegetable = db.define('vegetables', {
-    name: {
-        type: Sequelize.string,
-        allowNull: false
-    }, 
-    color: {
-        type: Sequelize.string,
-        allowNull: false
-    },
-    planted_on: {
-        type: Sequelize.date,
-        allowNull: false    
-    }
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  color: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  planted_on: {
+    type: Sequelize.DATE,
+    allowNull: false,
+  },
+});
+
+Plot.belongsTo(Gardener); // Plot.belongsTo(Garneder) -- Creates a Garnener ID in the Plots Table
+Gardener.hasOne(Plot); // Creates methods for Gardener to use
+Gardener.belongsTo(Vegetable, { as: 'Fav_Veg' }); //This is the fav vegetable of the gardner
+
+Vegetable.belongsToMany(Plot, { through: 'vegetable_plot' });
+Plot.belongsToMany(Vegetable, { through: 'vegetable_plot' });
+
+const potato = Vegetable.create( return {
+  name: 'potato',
+  color: 'red',
+  planted_on: 20180101,
 })
 
+
+module.exports = db;
--- seed.js
+++ seed.js
@@ -1,12 +1,12 @@
-const db = require('./models')
+const db = require('./models');
 
-db.sync({force: true})
-    .then (() => {
-        console.log('DB Synced. YEAH!')
-    })
-    .catch (err => {
-        console.log('messed up:', err)
-    })
-    .finally (()=> {
-        await db.close()
-    })
+db.sync({ force: true })
+  .then(() => {
+    console.log('DB Synced. YEAH!');
+  })
+  .catch(err => {
+    console.log('messed up:', err);
+  })
+  .finally(() => {
+    db.close();
+  });
