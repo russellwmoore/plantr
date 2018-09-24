0804d0f78df0c025dc92e5fbffacf0d5b65ad1e9
Russell Moore <russellwmoore@gmail.com>
2018-09-24 17:00:06 -0400

    Make db connection and begin models

--- models.js
+++ models.js
@@ -2,3 +2,16 @@ const Sequelize = require('sequelize')
 const db = new Sequelize('postgres://localhost:5432/plantr')
 
 module.exports = db
+
+const Gardener = db.define('gardner', {
+    name : {
+      type: Sequelize.STRING,
+      allownull: false
+    },
+    age: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    }
+}
+
+
