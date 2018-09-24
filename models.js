fe587171722bb6dd06c472217460da2289a395a2
Loren Mariquit <l.mariquit@gmail.com>
2018-09-24 17:27:35 -0400

    oawjief

--- models.js
+++ models.js
@@ -3,15 +3,40 @@ const db = new Sequelize('postgres://localhost:5432/plantr')
 
 module.exports = db
 
-const Gardener = db.define('gardner', {
+const Gardener = db.define('gardners', {
     name : {
-      type: Sequelize.STRING,
+        type: Sequelize.STRING,
       allownull: false
     },
     age: {
-      type: Sequelize.INTEGER,
+        type: Sequelize.INTEGER,
       allowNull: false
     }
-}
+})
 
+const Plot = db.define('plots', {
+    size: {
+        type: Sequelize.integer,
+        allowNull: false
+    },
+    shaded: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+    }
+})
+
+const Vegetable = db.define('vegetables', {
+    name: {
+        type: Sequelize.string,
+        allowNull: false
+    }, 
+    color: {
+        type: Sequelize.string,
+        allowNull: false
+    },
+    planted_on: {
+        type: Sequelize.date,
+        allowNull: false    
+    }
+})
 
